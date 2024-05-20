90e450ff163a70396a1af1302890b50983414eee
N0N3123 <bartoszbanach199@gmail.com>
2024-05-20 14:46:12 +0200

    2024-05-20 BB

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for
+import requests
+from bs4 import BeautifulSoup
+from . import utils
 
 @app.route('/')
 def index():
@@ -9,8 +12,17 @@ def index():
 def extract():
     if request.method=='POST':
         product_id = request.form.get('product_id')
-        return redirect(url_for('product', product_id=product_id))
+        url = f"https://www.ceneo.pl/{product_id}"
+        response = requests.get(url)
+        if response.status_code== requests.codes['ok']:
+            page_dom= BeautifulSoup(response.text,"html.parser")
+            opinions_count = utils.extract(page_dom , 'a.product-review__link > span')
+            if opinions_count:
+                return redirect(url_for('product', product_id=product_id))
+            return render_template("extract.html", error="Podany produkt nie ma żadnych opinii")
+        return render_template("extract.html", error="Produkt o takim kodzie nie istnieje")
     return render_template("extract.html")
+    
 
 @app.route('/products')
 def products():
